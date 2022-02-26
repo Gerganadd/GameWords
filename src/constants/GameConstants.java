e236de2534fc49157d2d8254f0c5374dd13ba085
Gerganadd <gerganadoncheva2003@gmail.com>
2022-02-26 21:37:33 +0200

    added new constants

--- src/constants/GameConstants.java
+++ src/constants/GameConstants.java
@@ -3,4 +3,8 @@ package constants;
 public class GameConstants
 {
 	public static final String FILE_PATH = "src/resources/words.xml";
+	public static final String COORDINATES_FILE_PATH = "src/resources/elementsCoordinates.xml";
+	
+	public static final String BACKGROUND_PICTURE_PATH = "src/resources/beach.png";
+	public static final String ELEMENT_PICTURE_PATH = "src/resources/closed-chest (1).png";
 }
---
+++ src/constants/ViewConstants.java
@@ -0,0 +1,12 @@
+package constants;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
+public class ViewConstants 
+{
+	public static final LayoutManager LAYOUT = new FlowLayout();
+	
+	public static final Dimension D_WORD_WINDOW = new Dimension(300, 400);
+}
