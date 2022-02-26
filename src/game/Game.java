5db5f5aa1d37b2867d3d8c7cf104813098110628
Gerganadd <gerganadoncheva2003@gmail.com>
2022-02-26 21:42:00 +0200

    created game class

---
+++ src/game/Game.java
@@ -0,0 +1,77 @@
+package game;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+import java.util.Map;
+
+import javax.swing.JFrame;
+
+import constants.GameConstants;
+import game_components.*;
+import parser.Parser;
+import windows.*;
+
+public class Game 
+{
+	private final LayoutManager LAYOUT = new FlowLayout();
+	
+	private static Game game = null;
+	
+	private Map<Word, Coordinate> info = Parser.parse(GameConstants.FILE_PATH); // change name
+	private JFrame window;
+	
+	
+	public Game()
+	{
+		window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setLayout(LAYOUT);
+	}
+	
+	public static Game getInstance()
+	{
+		if (game == null)
+		{
+			game = new Game();
+		}
+		
+		return game;
+	}
+	
+	public static void setSize(Component c, Dimension d)
+	{
+		c.setSize(d);
+		c.setPreferredSize(d);
+		c.setMinimumSize(d);
+		c.setMaximumSize(d);
+	}
+	
+	public void start()
+	{
+		window.add(new MainWindow());
+		
+		window.pack();
+		window.repaint();
+		
+		window.setVisible(true);
+	}
+	
+	public void addWordWindow(Word w) // to-do change name
+	{
+		window.getContentPane().removeAll();
+		
+		window.add(new WordWindow(w));
+		
+		window.pack();
+		window.repaint();
+	}
+	
+	public Map<Word, Coordinate> getInfo()
+	{
+		return info;
+	}
+	
+	
+}
