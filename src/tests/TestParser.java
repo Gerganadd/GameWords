7fb1f7cae504af0852e9cbe1a2f742a04735bc61
Gerganadd <gerganadoncheva2003@gmail.com>
2022-02-26 21:40:25 +0200

    tested parser

--- src/tests/TestParser.java
+++ src/tests/TestParser.java
@@ -1,16 +1,18 @@
 package tests;
 
-import java.util.List;
+import java.util.Map;
 
 import constants.GameConstants;
+import game_components.Coordinate;
 import game_components.Word;
 import parser.Parser;
 
 public class TestParser {
 
-	public static void main(String[] args) {
-		List<Word> words = Parser.parseWords(GameConstants.FILE_PATH);
-		words.forEach(x -> System.out.println(x));
+	public static void main(String[] args) 
+	{
+		Map<Word, Coordinate> info = Parser.parse(GameConstants.FILE_PATH);
+		info.forEach((k, v) -> System.out.println(k + " : " + v));
 	}
 
 }
