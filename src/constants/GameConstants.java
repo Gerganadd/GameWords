2e29f588dc398dc596f53a852b0964f4d4574a45
Gerganadd <gerganadoncheva2003@gmail.com>
2022-03-02 20:18:21 +0200

    added new constant

--- src/constants/GameConstants.java
+++ src/constants/GameConstants.java
@@ -7,4 +7,5 @@ public class GameConstants
 	
 	public static final String BACKGROUND_PICTURE_PATH = "src/resources/beach.png";
 	public static final String ELEMENT_PICTURE_PATH = "src/resources/closed-chest (1).png";
+	public static final String SELECTED_ELEMENT_PICTURE_PATH = "src/resources/open-chest (1).png";
 }
