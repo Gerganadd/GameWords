aadef02441fe74f2757c730a3f1bcf3e3917426a
Gerganadd <gerganadoncheva2003@gmail.com>
2022-03-08 18:05:14 +0200

    changed picture path

--- src/constants/GameConstants.java
+++ src/constants/GameConstants.java
@@ -6,6 +6,8 @@ public class GameConstants
 	
 	public static final String FILE_PATH = "src/resources/words.xml";
 	
+	public static final String PLAYER_PICTURE_PATH = "src/resources/player_700_400_1_cut.png";
+	
 	public static final String BACKGROUND_PICTURE_PATH = "src/resources/beach.png";
 	public static final String ELEMENT_PICTURE_PATH = "src/resources/closed-chest (1).png";
 	public static final String SELECTED_ELEMENT_PICTURE_PATH = "src/resources/open-chest (1).png";
